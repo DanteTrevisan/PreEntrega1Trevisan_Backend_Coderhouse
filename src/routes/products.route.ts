91e8af429e5954f27fd73b8c509229fbb3410ce9
Dante Trevisan <dante.trevisan248@gmail.com>
2025-02-15 23:33:13 -0300

    terminado endpoints, queda probar

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -28,6 +28,76 @@ productsRouter.get("/", async(req: Request, res: Response) => {
         limitParsed = parseInt(limit)
     }
     res.json(products.splice(0, limitParsed))
+});
+
+productsRouter.get("/:pid", async(req: Request, res:Response) => {
+    const productManager: ProductManager = new ProductManager(productsPath);
+    const pid: number = validateId(req.params.pid);
+    if (pid) {
+        const IdProduct: IdProduct = await productManager.getProductById(pid);
+        if (IdProduct) {
+            res.json(IdProduct);
+        } else {
+            res
+                .status(404)
+                .json(failureStatus(`el producto con id ${pid} no existe`))
+        }
+    } else {
+        res.status(404).json(failureStatus("Pid invalido."))
+    }
+});
+
+productsRouter.post("/", async(req: Request, res:Response) => {
+    const productManager: ProductManager = new ProductManager(productsPath);
+    const product: Product = req.body
+    await productManager.addProduct(product, (error: Error) => {
+        if (error) {
+            res.status(500).json(failureStatus(error.message));
+        } else {
+            res.json(successStatus)
+        }
+    });
+});
+
+productsRouter.put("/:pid", async(req:Request,res: Response) => {
+    const productManager: ProductManager = new ProductManager(productsPath);
+    const pid: number = validateId(req.params.id);
+    if (pid) {
+        const updateProperties: UpdateProduct = validateUpdateProduct(req.body);
+        if (updateProperties) {
+            await productManager.updateProduct(
+                pid,
+                updateProperties,
+                (error:Error) => {
+                    if (error) {
+                        res.status(500).json(failureStatus(error.message))
+                    } else {
+                        res.json(successStatus)
+                    }
+                }
+            )
+        } else {
+            res.status(404).json(failureStatus("Propiedades invalidas"))
+        }
+    } else {
+        res.status(404).json(failureStatus("Producto ID invalido"))
+    }
+});
+
+productsRouter.delete("/:pid", async(req: Request, res: Response) => {
+    const productManager: ProductManager = new ProductManager(productsPath);
+    const pid: number = validateId(req.params.pid);
+    if (pid) {
+        await productManager.deleteProduct(pid, (error: Error) => {
+            if (error) {
+                res.status(404).json(failureStatus(error.message));
+            } else {
+                res.json(successStatus)
+            }
+        });
+    } else {
+        res.status(404).json(failureStatus("Producto ID invalido."))
+    }
 })
 
 
